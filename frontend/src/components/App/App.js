cedf89b4f3673fc7316c87a3c261a4acf0b08ba3
dev-marko <markospasenovski00@gmail.com>
2022-03-23 01:37:51 +0100

    Hooks with useState, added addProduct and editProduct component, implemented pagination with the react-paginate package

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -5,18 +5,20 @@ import Manufacturers from "../Manufacturers/manufacturers";
 import Categories from "../Categories/categories";
 import Products from "../Products/ProductList/products";
 import Header from "../Header/header";
+import ProductAdd from "../Products/ProductAdd/productAdd";
+import ProductEdit from "../Products/ProductEdit/productEdit";
 import EShopService from "../../repository/eshopRepository";
 import {BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom";
-
 
 class App extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            manufacturers : [],
-            products : [],
-            categories : []
+            manufacturers: [],
+            products: [],
+            categories: [],
+            selectedProduct: {}
         }
     }
 
@@ -27,10 +29,26 @@ class App extends Component {
                     <Header/>
                     <div className={"container"}>
                         <Routes>
-                            <Route path={"/manufacturers"} element={<Manufacturers manufacturers = {this.state.manufacturers}/>} exact />
-                            <Route path={"/categories"} element={<Categories categories = {this.state.categories}/>} exact />
-                            <Route path={"/products"} element={<Products products = {this.state.products}/>} exact />
-                            <Route path="/" element={<Navigate replace to="/products" />} />
+                            <Route path={"/manufacturers"}
+                                   element={<Manufacturers manufacturers={this.state.manufacturers}/>} exact/>
+                            <Route path={"/categories"}
+                                   element={<Categories categories={this.state.categories}/>}
+                                   exact/>
+                            <Route path={"/products/add"}
+                                   element={<ProductAdd categories={this.state.categories}
+                                                        manufacturers={this.state.manufacturers}
+                                                        onAddProduct={this.addProduct}/>} exact/>
+                            <Route path={"/products/edit/:id"}
+                                   element={<ProductEdit categories={this.state.categories}
+                                                         manufacturers={this.state.manufacturers}
+                                                         onEditProduct={this.editProduct}
+                                                         product={this.state.selectedProduct}/>} exact/>
+                            <Route path={"/products"}
+                                   element={<Products products={this.state.products}
+                                                      onDelete={this.deleteProduct}
+                                                      onEdit={this.getProduct}/>}
+                                   exact/>
+                            <Route path="/" element={<Navigate replace to="/products"/>}/>
                         </Routes>
                     </div>
                 </main>
@@ -57,6 +75,13 @@ class App extends Component {
             });
     }
 
+    deleteProduct = (id) => {
+        EShopService.deleteProduct(id)
+            .then(() => {
+                this.loadProducts();
+            })
+    }
+
     loadProducts = () => {
         EShopService.fetchProducts()
             .then((data) => {
@@ -76,6 +101,29 @@ class App extends Component {
     //             })
     //         });
     // }
+
+    addProduct = (name, price, quantity, category, manufacturer) => {
+        EShopService.addProduct(name, price, quantity, category, manufacturer)
+            .then(() => {
+                this.loadProducts();
+            });
+    }
+
+    getProduct = (id) => {
+        EShopService.getProduct(id)
+            .then((data) => {
+                this.setState({
+                    selectedProduct: data.data
+                })
+            });
+    }
+
+    editProduct = (id, name, price, quantity, category, manufacturer) => {
+        EShopService.editProduct(id, name, price, quantity, category, manufacturer)
+            .then(() => {
+                this.loadProducts();
+            });
+    }
 
     componentDidMount() {
         this.loadManufacturers();
--- frontend/src/components/Products/ProductTerm/productTerm.js
+++ frontend/src/components/Products/ProductTerm/productTerm.js
@@ -1,6 +1,7 @@
 // Eden produkt vo ramki na soodvetnata products lista
 
 import React from "react";
+import {Link} from "react-router-dom";
 
 const productTerm = (props) => {
     return (
@@ -10,6 +11,19 @@ const productTerm = (props) => {
             <td scope={"col"}>{props.term.quantity}</td>
             <td scope={"col"}>{props.term.category.name}</td>
             <td scope={"col"}>{props.term.manufacturer.name}</td>
+            <td scope={"col"} className={"text-right"}>
+                <a
+                    title={"Delete"}
+                    className={"btn btn-danger"}
+                    onClick={() => props.onDelete(props.term.id)}>
+                    Delete
+                </a>
+                <Link className={"btn btn-info ms-2"}
+                      onClick={() => props.onEdit(props.term.id)}
+                      to={`/products/edit/${props.term.id}`}>
+                    Edit
+                </Link>
+            </td>
         </tr>
     );
 }
